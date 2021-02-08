39211318dff8b4d50daaceaefc1f9b848057d03f
Frederico Batista <frederico@Fredericos-MBP.station>
2021-02-08 20:43:13 +0100

    contact page implemented

--- pages/contato.js
+++ pages/contato.js
@@ -2,6 +2,8 @@ import { Client } from "utils/prismicHelpers";
 import Layout from "components/Layout/Layout";
 import Meta from "components/Meta/Meta";
 import { RichText } from "prismic-reactjs";
+import styles from "styles/pages/contato.module.scss";
+import Text from "components/Text/Text";
 
 const Contato = ({ doc, config }) => {
 	const contato = doc ? doc.data : null;
@@ -9,6 +11,23 @@ const Contato = ({ doc, config }) => {
 	return (
 		<Layout config={config}>
 			{contato && <Meta pageTitle={RichText.asText(contato.titulo)} />}
+			{contato && (
+				<section className={`${styles.section} grid grid--inner`}>
+					<header>
+						<h1 className={`h-1`}>{RichText.asText(contato.titulo)}</h1>
+					</header>
+					<ul className={styles.ways}>
+						{contato.formas.map((forma, key) => (
+							<li key={key}>
+								<dt>{forma.forma}</dt>
+								<dd>
+									<RichText render={forma.conteudo} />
+								</dd>
+							</li>
+						))}
+					</ul>
+				</section>
+			)}
 		</Layout>
 	);
 };
