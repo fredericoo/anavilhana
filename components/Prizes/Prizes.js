973a31ff2abc5b7eec9b8deabc8261dc36689549
Frederico Batista <frederico@Fredericos-MBP.station>
2021-02-09 16:26:49 +0100

    prizes and nav adjustments

--- components/Prizes/Prizes.js
+++ components/Prizes/Prizes.js
@@ -1,15 +1,22 @@
 import styles from "./Prizes.module.scss";
 import useTranslation from "next-translate/useTranslation";
 import Prize from "./Prize";
+import Button from "components/Button/Button";
+import { useState } from "react";
 
 const Prizes = ({ prizes }) => {
+	const [show, setShow] = useState(6);
 	if (!prizes || !prizes[0].premio_titulo) return null;
 	const { t } = useTranslation();
+	const showMore = () => {
+		setShow(prizes.length);
+	};
+
 	return (
 		<div className={`${styles.section} grid grid--inner`}>
 			<h2 className={`h-2 ${styles.heading}`}>{t("common:premiacoes")}</h2>
 			<div className={`${styles.prizes}`}>
-				{prizes.map((premio, key) => (
+				{prizes.slice(0, show).map((premio, key) => (
 					<Prize
 						key={key}
 						title={premio.premio_titulo}
@@ -18,6 +25,13 @@ const Prizes = ({ prizes }) => {
 					/>
 				))}
 			</div>
+			{show < prizes.length && (
+				<div className={styles.showMore}>
+					<Button type="link" onClick={showMore}>
+						{t("common:mais")} ({prizes.length})
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 };
