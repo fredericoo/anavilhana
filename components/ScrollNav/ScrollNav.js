46cfa6f9a679dda121c96c2e7671b5dd1aaddaf0
Frederico Batista <rincofrederico@gmail.com>
2021-03-01 19:30:49 +0100

    scroll to selected option on mobile

--- components/ScrollNav/ScrollNav.js
+++ components/ScrollNav/ScrollNav.js
@@ -1,11 +1,12 @@
 import styles from "./ScrollNav.module.scss";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import animateScrollTo from "animated-scroll-to";
 
 const ScrollNav = ({ items }) => {
 	const [activeNav, setActiveNav] = useState("");
 	const [navItems, setNavItems] = useState([]);
 	const [overlay, setOverlay] = useState(false);
+	const navRef = useRef();
 
 	useEffect(() => {
 		setNavItems(
@@ -19,15 +20,6 @@ const ScrollNav = ({ items }) => {
 		);
 	}, []);
 
-	const handleScroll = () => {
-		let active = null;
-		navItems.forEach(({ ref, id }) => {
-			const rect = ref.getBoundingClientRect();
-			if (rect.top <= window.innerHeight / 2) active = id;
-		});
-		if (active && active !== activeNav) setActiveNav(active);
-	};
-
 	const handleOverlay = (entries, _) => {
 		let overlay = false;
 		entries.forEach((entry) => {
@@ -37,20 +29,47 @@ const ScrollNav = ({ items }) => {
 	};
 
 	useEffect(() => {
-		handleScroll();
-		document.addEventListener("scroll", () => handleScroll());
+		if (navRef.current && !!activeNav.length) {
+			const selectedOption = navRef.current.querySelector(
+				`li[data-scroll=${activeNav}]`
+			);
+			animateScrollTo(selectedOption, {
+				elementToScroll: navRef.current,
+			});
+		}
+	}, [activeNav]);
 
-		const observer = new window.IntersectionObserver(handleOverlay, {
-			root: null,
-			threshold: [0.5],
-		});
-		navItems
-			.filter(({ ref }) => ref.dataset.hidenav)
-			.forEach(({ ref }) => observer.observe(ref));
+	useEffect(() => {
+		if (typeof window === "undefined") return () => {};
+		let observer;
+		const handleScroll = () => {
+			let active = null;
+			navItems.forEach(({ ref, id }) => {
+				const rect = ref.getBoundingClientRect();
+				if (rect.top <= window.innerHeight / 2) active = id;
+			});
+			if (active && active !== activeNav) {
+				setActiveNav(active);
+			}
+		};
+		if (!!navItems.length) {
+			handleScroll();
+			window.addEventListener("scroll", () => handleScroll());
+			observer = new window.IntersectionObserver(handleOverlay, {
+				root: null,
+				threshold: [0.5],
+			});
+			navItems
+				.filter(({ ref }) => ref.dataset.hidenav)
+				.forEach(({ ref }) => observer.observe(ref));
+		}
 
 		return () => {
-			document.removeEventListener("scroll", handleScroll);
-			observer.disconnect();
+			if (!!navItems.length) {
+				console.log("removing listener");
+				window.removeEventListener("scroll", handleScroll);
+				observer.disconnect();
+			}
 		};
 	}, [navItems]);
 
@@ -59,9 +78,12 @@ const ScrollNav = ({ items }) => {
 	};
 
 	return (
-		<ul className={`${styles.list} ${overlay ? styles.hidden : ""}`}>
+		<ul
+			ref={navRef}
+			className={`${styles.list} ${overlay ? styles.hidden : ""}`}
+		>
 			{navItems.map(({ ref, id, label }, key) => (
-				<li key={key}>
+				<li data-scroll={id} key={key}>
 					<button
 						onClick={() => scrollTo(ref)}
 						type="button"
