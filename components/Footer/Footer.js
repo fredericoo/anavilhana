997edd59dc3facc21c82d66d5853d1982948fe25
Frederico Batista <rincofrederico@gmail.com>
2021-03-01 21:10:21 +0100

    refactored footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,40 +3,47 @@ import { default as NextLink } from "next/link";
 import useTranslation from "next-translate/useTranslation";
 import { RichText } from "prismic-reactjs";
 import { hrefResolver } from "prismic-configuration";
+import Grid from "components/Grid/Grid";
 
 const Footer = ({ config }) => {
 	const { t } = useTranslation();
 
 	return (
-		<footer className={`${styles.section} grid grid--inner`}>
+		<footer className={`${styles.section}`}>
 			<h2 className="visually-hidden">{t("common:rodape")}</h2>
-			<div className={styles.summary}>
-				{config && <RichText render={config.data.resumo} />}
-				{config && (
-					<div className={styles.social}>
-						{config.data.social.map((item, index) => (
-							<NextLink key={index} href={hrefResolver(item.link)} passHref>
-								<a>
-									<img src={item.icone.url} />
-								</a>
-							</NextLink>
-						))}
-					</div>
-				)}
-			</div>
-			<ul className={styles.footerMenu}>
-				{config &&
-					config.data.menu_rodape.map((item, index) => (
-						<li key={index}>
-							<NextLink href={hrefResolver(item.link)} passHref>
-								<a>{item.opcao}</a>
-							</NextLink>
-						</li>
-					))}
-			</ul>
-			<div className={styles.address}>
-				{config && <RichText render={config.data.endereco} />}
-			</div>
+			<Grid>
+				<Grid.Col lg="grid-start / col-7" className="s-sm">
+					{config && <RichText render={config.data.resumo} />}
+					{config && (
+						<div className={styles.social}>
+							{config.data.social.map((item, index) => (
+								<NextLink key={index} href={hrefResolver(item.link)} passHref>
+									<a>
+										<img src={item.icone.url} />
+									</a>
+								</NextLink>
+							))}
+						</div>
+					)}
+				</Grid.Col>
+				<Grid.Col lg="span 3">
+					<ul className={`${styles.menu} s-sm`}>
+						{config &&
+							config.data.menu_rodape.map((item, index) => (
+								<li key={index}>
+									<NextLink href={hrefResolver(item.link)} passHref>
+										<a>{item.opcao}</a>
+									</NextLink>
+								</li>
+							))}
+					</ul>
+				</Grid.Col>
+				<Grid.Col lg="span 3">
+					<address className="s-sm l-2">
+						{config && <RichText render={config.data.endereco} />}
+					</address>
+				</Grid.Col>
+			</Grid>
 		</footer>
 	);
 };
